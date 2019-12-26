5fbe5e8b435924a3c036fadec95e107f84ca2af9
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-12-26 15:15:11 -0800

    Generalize TableModel to hold more than just partitions.

--- main.py
+++ main.py
@@ -22,13 +22,34 @@ class Partition(Atom):
 
     scale = Str()
 
+class SubstitutionModel(Atom):
+    model = Str()
 
-class TableModel(Atom):
-    """A list of partitions
+    alphabet = Str()
+
+class IndelModel(Atom):
+    model = Str()
+
+class ScaleModel(Atom):
+    model = Str()
+
+class Alphabet(Atom):
+    model = Str()
+
+class BranchLengthModel(Atom):
+    model = Str()
+
+class ATModel(Atom):
+    """A collection of partitions that can share models
 
     """
-    partitions = ContainerList(Partition)
 
+    partitions = ContainerList(Partition)
+    smodels = ContainerList(SubstitutionModel)
+    imodels = ContainerList(IndelModel)
+    scales = ContainerList(ScaleModel)
+
+    # branch_lengths = BranchLengthModel()
     def add_partition(self):
         partition = Partition(filename = '25-muscle.fasta', alphabet = 'RNA', substitution_model = 'tn93', indel_model = 'rs07', scale = '~gamma[0.5,2]')
         self.partitions.append(partition)
@@ -37,13 +58,14 @@ class TableModel(Atom):
         if len(self.partitions) > 0:
             self.partitions.pop()
 
+
 def main():
 
     with enaml.imports():
         from partition_view import PartitionView
         from partition_view import PartitionsTab
 
-    data_model = TableModel(partitions=[])
+    data_model = ATModel(partitions=[], smodels=[], imodels=[], scales=[])
 
     app = QtApplication()
     # Create a view and show it.
